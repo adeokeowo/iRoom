a970888b294bcc5c9c1bde980137314316dae0b8
Adedayo Okeowo <aokeowo@AdeMacBookPro.local>
2018-09-11 00:30:21 +0100

    create iRoom-Register project commit

---
+++ iRoom-Register/iRoom-Register/iRoom-Register/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRoom-Register
+//
+//  Created by Adedayo Okeowo on 11/09/2018.
+//  Copyright © 2018 Adedayo Okeowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

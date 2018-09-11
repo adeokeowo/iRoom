82c5f3e0d34be843cc06f9febc63bb7b0d8dc8c2
Adedayo Okeowo <aokeowo@AdeMacBookPro.local>
2018-09-11 10:46:52 +0100

    create iRoom-Register events commit

--- iRoom-Register/iRoom-Register/iRoom-Register/ViewController.swift
+++ iRoom-Register/iRoom-Register/iRoom-Register/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: constants
+    var titleValues = ["Mr", "Miss", "Mrs"]
+    let cellReuseIdentifier = "cell"
+    //MARK: components
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var labelUsername: UILabel!
+    @IBOutlet weak var textFieldPwd: UITextField!
+    @IBOutlet weak var labelPwd: UILabel!
+    @IBOutlet weak var textFieldConfirmPwd: UITextField!
+    @IBOutlet weak var labelConfirmPwd: UILabel!
+    @IBOutlet weak var textFieldTitle: UITextField!
+    @IBOutlet weak var textFieldFName: UITextField!
+    @IBOutlet weak var textFieldLName: UITextField!
+    @IBOutlet weak var textFieldDOBPicker: UIDatePicker!
+    @IBOutlet weak var labelMsg: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,60 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func eventEmail(_ sender: UITextField) {
+        if isValid(textFieldUsername.text!){
+            labelUsername.text = "Email is valid."
+        } else {
+            labelUsername.text = "Email is not valid."
+        }
+    }
+    
+    @IBAction func eventPwd(_ sender: UITextField) {
+        if isValidPassword(testStr: textFieldPwd.text){
+            labelPwd.text = "Password is valid."
+        } else {
+            labelPwd.text = "Password is not valid."
+        }
+    }
+    
+    @IBAction func eventConfirmPwd(_ sender: UITextField) {
+        if ( (textFieldPwd.text?.elementsEqual(textFieldConfirmPwd.text!))! != true ){
+            labelConfirmPwd.text = "Password don't match."
+        } else {
+            labelConfirmPwd.text = "Passwords match."
+        }
+    }
+    
+    
+    //MARK: functions
+    /// Validate email string
+    ///
+    /// - parameter email: A String that rappresent an email address
+    ///
+    /// - returns: A Boolean value indicating whether an email is valid.
+    func isValid(_ email: String) -> Bool {
+        let emailRegEx = "(?:[a-zA-Z0-9!#$%\\&‘*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}" +
+        "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+        "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
+        "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
+        "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+        "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES[c] %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func isValidPassword(testStr:String?) -> Bool {
+        guard testStr != nil else { return false }
+        // at least one uppercase,
+        // at least one digit
+        // at least one lowercase
+        // 8 characters total
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}")
+        return passwordTest.evaluate(with: testStr)
+    }
 
 }
 
